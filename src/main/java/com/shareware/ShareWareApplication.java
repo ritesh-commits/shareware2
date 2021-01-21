544db3fb73dd8c12ace5eea125f2e92beeeee7a7
ritesh-commits <68008524+ritesh-commits@users.noreply.github.com>
2021-01-21 14:17:14 +0530

    fix

--- src/main/java/com/shareware/ShareWareApplication.java
+++ src/main/java/com/shareware/ShareWareApplication.java
@@ -1,20 +1,25 @@
+package com.shareware;
+
 import com.shareware.uploadingfiles.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 public class ShareWareApplication {
+
+    @Resource
+    StorageService storageService;
+
     public static void main(String[] args) {
         SpringApplication.run(ShareWareApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(StorageService storageService) {
-        return (args) -> {
-            storageService.deleteAll();
-            storageService.init();
-        };
-    }
+//    @Override
+//    public void run(String... arg) throws Exception {
+//        storageService.deleteAll();
+//        storageService.init();
+//    }
 }
